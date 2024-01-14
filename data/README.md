5c85337b67fe210470469a6bc8b81aacc2ac7ffa
noogen <tom@noogen.net>
2024-01-13 18:07:04 -0600

    add tls support

--- data/README.md
+++ data/README.md
@@ -1,2 +1,15 @@
 # Docker data folder
-This is where all configuration of Haraka are saved.
+This is where all configuration of Haraka are saved for running with docker-compose/docker container.
+
+## Config
+This allow you to update and save your haraka/config folder.
+
+## resque
+This is the directory that resque use to output full eml file before it get sends to the API
+
+
+- `myinit.sh` - file is use by docker to initialize the config folder.
+- `mystart.sh` - this is use to override default docker startup command to use with the `instrumentisto/haraka` docker image.
+
+
+
--- data/myinit.sh
+++ data/myinit.sh
@@ -7,8 +7,28 @@ cd /etc/haraka
 if [ ! -f /data/config/plugins ]; then
   # cp -R -u -p /1defaults/plugins/. /data/plugins
   # cp -R -u -p plugins/. /data/plugins
- 
+
+
+  # copy our default config file
   cp -R -u -p /1defaults/config/. /data/config
+
+  # copy existing config over to data config
   cp -R -u -p config/. /data/config
+
+  # copy our resque.ini to /data/config
   cp -R -u -p /1config/resque.ini /data/config
+fi
+
+# https://stackoverflow.com/questions/10175812/how-to-generate-a-self-signed-ssl-certificate-using-openssl
+SSL_DIR="/data/config"
+HOST=$HOSTNAME
+
+# Generate dummy self-signed certificate.
+if [ ! -f $SSL_DIR/tls_cert.pem ] || [ ! -f $SSL_DIR/tls_key.pem ]
+then
+  echo "Generating dummy SSL certificate..."
+  openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
+  -nodes -keyout $SSL_DIR/tls_key.pem -out $SSL_DIR/tls_cert.pem -subj "/CN=$HOST" \
+  -addext "subjectAltName=DNS:$HOST,DNS:*.$HOST,IP:10.0.0.1"
+  echo "Complete"
 fi
