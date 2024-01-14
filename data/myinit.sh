7637f8e0dafd605322b6390e669dd34b824de60b
noogen <tom@noogen.net>
2024-01-14 09:02:19 -0600

    switch to generate ssl on running docker instead

--- data/myinit.sh
+++ data/myinit.sh
@@ -18,17 +18,3 @@ if [ ! -f /data/config/plugins ]; then
   # copy our resque.json to /data/config
   cp -R -u -p /1config/resque.json /data/config
 fi
-
-# https://stackoverflow.com/questions/10175812/how-to-generate-a-self-signed-ssl-certificate-using-openssl
-SSL_DIR="/data/config"
-HOST=$HOSTNAME
-
-# Generate dummy self-signed certificate.
-if [ ! -f $SSL_DIR/tls_cert.pem ] || [ ! -f $SSL_DIR/tls_key.pem ]
-then
-  echo "Generating dummy SSL certificate..."
-  openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
-  -nodes -keyout $SSL_DIR/tls_key.pem -out $SSL_DIR/tls_cert.pem -subj "/CN=$HOST" \
-  -addext "subjectAltName=DNS:$HOST,DNS:*.$HOST,IP:10.0.0.1"
-  echo "Complete"
-fi
--- data/mystart.sh
+++ data/mystart.sh
@@ -13,4 +13,18 @@ grep -qxF 'resque' config/plugins || echo "resque" >> config/plugins
 
 echo $HOSTNAME > /etc/haraka/config/me
 
+# https://stackoverflow.com/questions/10175812/how-to-generate-a-self-signed-ssl-certificate-using-openssl
+SSL_DIR="/etc/haraka/config"
+HOST=$HOSTNAME
+
+# Generate dummy self-signed certificate.
+if [ ! -f $SSL_DIR/tls_cert.pem ] || [ ! -f $SSL_DIR/tls_key.pem ]
+then
+  echo "Generating dummy SSL certificate..."
+  openssl req -x509 -newkey rsa:4096 -sha256 -days 3650 \
+  -nodes -keyout $SSL_DIR/tls_key.pem -out $SSL_DIR/tls_cert.pem -subj "/CN=$HOST" \
+  -addext "subjectAltName=DNS:$HOST,IP:10.0.0.1"
+  echo "Complete"
+fi
+
 exec /usr/local/bin/haraka -c /etc/haraka
